f745170bd75f6d500bfaa51153f6247bf57f63b9
Vitaly Pyanzin <vitaly97w@gmail.com>
2022-06-18 18:36:00 +1000

    edit

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,9 +1,8 @@
 module.exports = {
   mongoURL:
-    process.env.MONGO_URL ||
-    'mongodb+srv://vital:spider45@cluster0.b3wzj.mongodb.net/vezdecode',
+    process.env.MONGO_URL,
   port: process.env.PORT || 5000,
-  jwt: process.env.JWT || "qwerty",
+  jwt: process.env.JWT,
   email: process.env.EMAIL,
   password: process.env.PASSWD,
 }
